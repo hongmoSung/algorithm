b50ef1b14b9d66268f24c54273dbcb8358001e9a
hongmoSung <tesung30@naver.com>
2022-10-22 13:31:13 +0900

    feature: 두 수의 차

---
+++ src/test/java/programmers/lv0/DiffTwoNumbersTest.java
@@ -0,0 +1,41 @@
+package programmers.lv0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiffTwoNumbersTest {
+
+    private final DiffTwoNumbers diffTwoNumbers = new DiffTwoNumbers();
+
+    /**
+     * <Table>
+     * <Thead>
+     * <tr>
+     * <th>num1</th>
+     * <th>num2</th>
+     * <th>result</th>
+     * </tr>
+     * </Thead>
+     * <Tbody>
+     * <tr>
+     * <th>2</th>
+     * <th>3</th>
+     * <th>-1</th>
+     * </tr>
+     * <tr>
+     * <th>100</th>
+     * <th>2</th>
+     * <th>98</th>
+     * </tr>
+     * </Tbody>
+     * </Table>
+     */
+    @Test
+    void diffTwoNumbersTest() {
+        assertThat(diffTwoNumbers.solution(2, 3)).isEqualTo(-1);
+        assertThat(diffTwoNumbers.solution(100, 2)).isEqualTo(98);
+    }
+
+}
