d75acd3dfa767b40779257b1ab0bcbeea5b0e490
hongmoSung <tesung30@naver.com>
2022-11-14 17:17:10 +0900

    feature: 문자열 뒤집기

---
+++ src/test/java/programmers/lv0/FlipStringTest.java
@@ -0,0 +1,33 @@
+package programmers.lv0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FlipStringTest {
+
+    private final FlipString flipString = new FlipString();
+
+    /**
+     * <table>
+     *     <tr>
+     *         <td>my_string</td>
+     *         <td>return</td>
+     *     </tr>
+     *     <tr>
+     *         <td>"jaron"</td>
+     *         <td>"noraj"</td>
+     *     </tr>
+     *     <tr>
+     *         <td>"bread"</td>
+     *         <td>"daerb"</td>
+     *     </tr>
+     * </table>
+     */
+    @Test
+    void flipString() {
+        assertThat(flipString.solution("jaron")).isEqualTo("noraj");
+        assertThat(flipString.solution("bread")).isEqualTo("daerb");
+    }
+
+}
