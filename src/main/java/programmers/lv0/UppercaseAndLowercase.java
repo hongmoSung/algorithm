11249d85f8f7c02e8b0c34002571a99581472e47
hongmoSung <tesung30@naver.com>
2022-11-22 16:49:51 +0900

    feature: 대문자와 소문자

---
+++ src/main/java/programmers/lv0/UppercaseAndLowercase.java
@@ -0,0 +1,21 @@
+package programmers.lv0;
+
+/**
+ * <p>문자열 my_string 이 매개변수로 주어질 때,</p>
+ * <p>대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return 하도록 solution 함수를 완성해주세요.</p>
+ */
+public class UppercaseAndLowercase {
+
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char s : my_string.toCharArray()) {
+            if (Character.isUpperCase(s)) {
+                answer.append(String.valueOf(s).toLowerCase());
+            } else {
+                answer.append(String.valueOf(s).toUpperCase());
+            }
+        }
+        return answer.toString();
+    }
+}
