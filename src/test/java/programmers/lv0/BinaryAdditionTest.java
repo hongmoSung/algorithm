c2315924f35d9bc0c4f7dd0bdbed5402ee9faf29
hongmoSung <tesung30@naver.com>
2022-12-05 18:02:42 +0900

    feature: 이진수 더하기

---
+++ src/test/java/programmers/lv0/BinaryAdditionTest.java
@@ -0,0 +1,36 @@
+package programmers.lv0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinaryAdditionTest {
+
+    private final BinaryAddition binaryAddition = new BinaryAddition();
+
+    /**
+     * <table>
+     *     <tr>
+     *         <td>bin1</td>
+     *         <td>bin2</td>
+     *         <td>result</td>
+     *     </tr>
+     *     <tr>
+     *         <td>"10"</td>
+     *         <td>"11"</td>
+     *         <td>"101"</td>
+     *     </tr>
+     *     <tr>
+     *         <td>"1001"</td>
+     *         <td>"1111"</td>
+     *         <td>"11000"</td>
+     *     </tr>
+     * </table>
+     */
+    @Test
+    void binaryAdditionTest() {
+        assertThat(binaryAddition.solution("10", "11")).isEqualTo("101");
+        assertThat(binaryAddition.solution("1001", "1111")).isEqualTo("11000");
+    }
+
+}
