8f7a05afaee15edb012f736a5803bf01b945581c
hongmoSung <tesung30@naver.com>
2022-12-07 11:27:20 +0900

    feature: 구슬을 나누는 경우의 수

---
+++ src/test/java/programmers/lv0/NumberOfWaysToDivideMarblesTest.java
@@ -0,0 +1,35 @@
+package programmers.lv0;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberOfWaysToDivideMarblesTest {
+
+    private final NumberOfWaysToDivideMarbles numberOfWaysToDivideMarbles = new NumberOfWaysToDivideMarbles();
+
+    /**
+     * <table>
+     *     <tr>
+     *         <td>balls</td>
+     *         <td>share</td>
+     *         <td>result</td>
+     *     </tr>
+     *     <tr>
+     *         <td>3</td>
+     *         <td>2</td>
+     *         <td>3</td>
+     *     </tr>
+     *     <tr>
+     *         <td>5</td>
+     *         <td>3</td>
+     *         <td>10</td>
+     *     </tr>
+     * </table>
+     */
+    @Test
+    void numberOfWaysToDivideMarblesTest() {
+        assertThat(numberOfWaysToDivideMarbles.solution(3, 2)).isEqualTo(3);
+        assertThat(numberOfWaysToDivideMarbles.solution(5, 3)).isEqualTo(10);
+    }
+}
